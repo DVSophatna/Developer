0dd7c551d9d72e280d3279fe38f3b5bd5aab813b
DVSophatna <csophatna@gmail.com>
2015-09-06 10:11:27 +0700

    Inital commit
    Hello GitHub

---
+++ controller/indexController.php
@@ -0,0 +1,11 @@
+<?php
+
+Class indexController Extends baseController {
+
+	public function index() {
+		$this->registry->template->welcome = 'Welcome to PHPRO MVC';
+		$this->registry->template->show('index');
+	}
+}
+
+?>
